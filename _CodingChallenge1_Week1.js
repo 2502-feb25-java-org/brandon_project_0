3b9eb2df98bb9ab6d93df147cfe6fde1231784f0
brandon-fox <bfox@foxtrot.email>
2019-03-04 09:11:39 -0500

    Brute Force Approach

---
+++ _CodingChallenge1_Week1.js
@@ -0,0 +1,15 @@
+function fizzbuzz(number) {
+    let result = "";
+    if (number % 3 === 0 && number % 5 === 0){
+        return "Fizz Buzz";
+    }
+    else if (number % 3 === 0){
+        return "Fizz";
+    }
+    else if (number % 5 === 0){
+        return "Buzz";
+    }
+    else {
+        return String(number);
+    }
+}
