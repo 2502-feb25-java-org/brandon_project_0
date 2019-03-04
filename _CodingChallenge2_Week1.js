a6af12c5e2896b1775dbc5de298b690e0ee9069a
brandon-fox <bfox@foxtrot.email>
2019-03-04 09:25:50 -0500

    Test up to 25 for Fib

--- _CodingChallenge2_Week1.js
+++ _CodingChallenge2_Week1.js
@@ -6,9 +6,7 @@ function f(number) {
         return f(number-1) + f(number-2);
     }
 }
-console.log(f(0));
-console.log(f(1));
-console.log(f(2));
-console.log(f(3));
-console.log(f(4));
-console.log(f(5));
+
+for (let i = 0; i < 25; i++) {
+    console.log(f(i));
+}
